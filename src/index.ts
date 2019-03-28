07f92174bba9c5308c51248195eac5d77fe1096e
Diego Stratta <strattadb@gmail.com>
2019-03-28 20:22:18 -0300

    fix: export classes and types

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,19 @@ export {
   makeParametizedQueryString,
   makeUnsafeRawQueryString,
 } from './queryBuilders';
+export {
+  Client,
+  Pool,
+  PoolClient,
+  ClientConnectionOptions,
+  ClientOptions,
+  PoolClientOptions,
+  PoolOptions,
+  Transaction,
+  TransactionFunction,
+  TransactionIsolationLevel,
+  TransactionOptions,
+  BaseClient,
+} from './clients';
 export { createClient, createPool } from './factories';
 export { PostreError, NoRowsError, MultipleRowsError } from './errors';
